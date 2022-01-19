947a7079f32fb755b402e1e7fba95024a1f728fe
Xstasy03 <95896830+Xstasy03@users.noreply.github.com>
2022-01-19 00:04:58 -0500

    Add files via upload

---
+++ scrpt.js
@@ -0,0 +1,16 @@
+const startButton = document.getElementById('start-button')
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    console.log('started')
+    startButton.classList.add('hide')
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
